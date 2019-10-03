b4bd9fa7c8a9b893ccc3ebaec11f2df5c886c95a
Radik <berezhnoy.rodion.andr0id@gmail.com>
2019-10-03 18:34:51 +0300

    Update

--- app/src/main/java/ru/startandroid/lesson44simplelistevents/MainActivity.java
+++ app/src/main/java/ru/startandroid/lesson44simplelistevents/MainActivity.java
@@ -18,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //нахожу view по id
         lvMain = (ListView) findViewById(R.id.lvMain);
-
+        //вешаю адаптер
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.names, android.R.layout.simple_list_item_1);
+        //сетим данные из адаптера в lvMain
         lvMain.setAdapter(adapter);
-
+        //листенер для нажатия на пункты списка
         lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -32,24 +33,24 @@ public class MainActivity extends AppCompatActivity {
                         + id);
             }
         });
-
+        //листенер для выделения пунктов
         lvMain.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 Log.d(LOG_TAG, "itemSelect: position = " + position + ", id = "
                         + id);
             }
-
+            
             public void onNothingSelected(AdapterView<?> parent) {
                 Log.d(LOG_TAG, "itemSelect: nothing");
             }
         });
-
+        //листенер для скроллов
         lvMain.setOnScrollListener(new AbsListView.OnScrollListener() {
             public void onScrollStateChanged(AbsListView view, int scrollState) {
                 Log.d(LOG_TAG, "scrollState = " + scrollState);
             }
-
+            //логи для разного состояния скроллов
             public void onScroll(AbsListView view, int firstVisibleItem,
                                  int visibleItemCount, int totalItemCount) {
                 Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
